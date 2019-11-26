fbf43dd54e7501a101a09a8b5d6f84dd37f38054
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-26 16:26:03 -0300

    Create initial Program.cs

--- aula196-interfaces2/Program.cs
+++ aula196-interfaces2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using aula196_interfaces2.Entities;
 
 namespace aula196_interfaces2
 {
@@ -6,7 +8,18 @@ namespace aula196_interfaces2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: " );
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractTotalValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Contract contract = new Contract(contractNumber, contractDate, contractTotalValue);
+            Console.WriteLine($"contract number: {contract.Number}, date: {contract.Date}, total value: {contract.TotalValue}");
+            Console.Write("Enter number of installments: ");
+            int installments = int.Parse(Console.ReadLine());
+        
         }
     }
 }
