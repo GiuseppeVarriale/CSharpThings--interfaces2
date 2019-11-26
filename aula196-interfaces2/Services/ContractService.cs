9f47ab0c93daef575a9acfdaea648511aa044c3f
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-26 18:21:56 -0300

    Add new services functions

---
+++ aula196-interfaces2/Services/ContractService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using aula196_interfaces2.Entities;
+namespace aula196_interfaces2.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContracts(Contract contract, int months)
+        {
+            for (int i = 1; i >= months; i++)
+            {
+                double basicQuota = contract.TotalValue / 3;
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
--- aula196-interfaces2/Services/OnlinePaymentService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace aula196_interfaces2.Services
-{
-    interface OnlinePaymentService
-    {
-        public double PaymentFee(double amount);
-        public double Interest(double amount, int months);
-        
-    }
-}
