8ea6e81a9fa4b062942071bce6375df27ffdaf80
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-26 19:31:19 -0300

    Add logic to Program.cs

--- aula196-interfaces2/Program.cs
+++ aula196-interfaces2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using aula196_interfaces2.Entities;
+using aula196_interfaces2.Services;
 
 namespace aula196_interfaces2
 {
@@ -9,17 +10,26 @@ namespace aula196_interfaces2
         static void Main(string[] args)
         {
             Console.WriteLine("Enter contract data");
-            Console.Write("Number: " );
+            Console.Write("Number: ");
             int contractNumber = int.Parse(Console.ReadLine());
             Console.Write("Date (dd/MM/yyyy): ");
-            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy", CultureInfo.InvariantCulture);
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
             Console.Write("Contract value: ");
             double contractTotalValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Contract contract = new Contract(contractNumber, contractDate, contractTotalValue);
-            Console.WriteLine($"contract number: {contract.Number}, date: {contract.Date}, total value: {contract.TotalValue}");
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractTotalValue);
+
             Console.Write("Enter number of installments: ");
-            int installments = int.Parse(Console.ReadLine());
-        
+            int months = int.Parse(Console.ReadLine());
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContracts(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment x in myContract.Installments)
+            {
+                Console.WriteLine($"{x.DueDate.ToString("dd/MM/yy")} - {x.Amount.ToString("F2", CultureInfo.InvariantCulture)} ");
+            }
         }
     }
 }
