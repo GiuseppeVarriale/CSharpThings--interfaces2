49c18a0c5bc01fd11b06699fefd8acfdbfd03520
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-26 17:36:49 -0300

    Add PaypalService Interface

---
+++ aula196-interfaces2/Services/PaypalService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aula196_interfaces2.Services
+{
+    class PaypalService : OnlinePaymentService
+    {
+        private const double _feePercentage = 0.02;
+        public const double _fonthlyInterest = 0.01;
+        public double Interest(double amount, int months)
+        {
+            return amount * _fonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * _feePercentage;
+        }
+    }
+}
